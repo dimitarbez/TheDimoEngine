99e8b0d7386c6da1ad65458a2763788a2d790676
Dimo-Desktop <dimitarbez@gmail.com>
2019-06-09 18:13:39 +0200

    Meshes

--- TheDimoEngine.cpp
+++ TheDimoEngine.cpp
@@ -2,6 +2,7 @@
 #include "GL/glew.h"
 #include "Display.h"
 #include "Shader.h"
+#include "Mesh.h"
 #undef main
 
 int main()
@@ -10,10 +11,18 @@ int main()
 	
 	Shader shader("./res/basicShader");
 
+	Vertex vertices[] = {	Vertex(glm::vec3(-0.5, -0.5, 0)),
+							Vertex(glm::vec3(0, 0.5, 0)), 
+							Vertex(glm::vec3(0.5, -0.5, 0))};
+
+	Mesh mesh(vertices, sizeof(vertices) / sizeof(vertices[0]));
+
 	while (!display.IsClosed())
 	{
 		glClearColor(0.0f, 0.3f, 0.2f, 1.0f);
 		shader.Bind();
+
+		mesh.Draw();
 
 		display.Update();
 	}
